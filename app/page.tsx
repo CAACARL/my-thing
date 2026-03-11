8d1224a1ec79098941b8f1e00ce342611ac94afb
@carloaalbao <wazzy42069@gmail.com>
2026-03-11 10:55:23 +0800

    womp

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,21 @@ import { Contact } from "@/components/Contacts/Index";
 import { Skills } from "@/components/Skills";
 import { Projects } from "@/components/Projects/Index";
 import { Experience } from "@/components/Experience/index";
+import { FadeIn } from "@/components/FadeIn";
 
 export default function Home() {
   return (
     <>
       {/* Hero section */}
-      <Hero />
+      <div className="flex items-center gap-8">
+        <Hero />
+        <FadeIn>
+          {/* Fixed oval container */}
+          <div className="relative w-64 h-80 mt-20 border-2 border-[#e53935] rounded-[50%/40%] overflow-hidden shadow-sm">
+            <img src="tenshi.jpg" alt="League streamer" className="w-full h-full object-cover" />
+          </div>
+        </FadeIn>
+      </div>
       {/* About section */}
       <About />
       {/* Skills section */}
