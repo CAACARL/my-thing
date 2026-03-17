0bfaddfca9b96778824c73bbcb5c70dd947c4c53
@carloaalbao <wazzy42069@gmail.com>
2026-03-17 11:15:20 +0800

    Finale 2

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -19,9 +19,9 @@ export function Hero() {
       <div className="w-full">
         <FadeIn>
           <h1 className="text-4xl md:text-5xl font-medium text-black tracking-tight">
-            Carlo Albao
+            Carlo Alberto D. Albao
           </h1>
-          <div className="w-30 h-0.5 bg-[#E53935] mt-4 mb-6" />
+          <div className="w-115 h-0.5 bg-[#E53935] mt-4 mb-6" />
         </FadeIn>
 
         <FadeIn delay={0.1}>
