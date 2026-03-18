2efda6f90cf329ec51ec68e4005735dba34b640e
@carloaalbao <wazzy42069@gmail.com>
2026-03-18 14:35:48 +0800

    T4

--- components/About/Index.tsx
+++ components/About/Index.tsx
@@ -9,7 +9,7 @@ export function About() {
       <FadeIn>
         <h2 className="text-xs uppercase tracking-widest text-[#E53935] font-medium mb-8">About</h2>
         <p className="text-base text-black-700 leading-loose w-full md:max-w-2xl">
-          Hi, I’m Carl, an aspiring web developer with a strong interest in building practical and
+          Hi, I’m Carlo, an aspiring web developer with a strong interest in building practical and
           user-friendly web applications. I’m always eager to learn new technologies and improve my
           skills through hands-on projects. Through continuous learning and practice, I aim to grow
           and create efficient and reliable web systems.
